a7f4bee73a6610698cf498daa72576920b1fbe62
LongDH20181591 <longx8qd@gmail.com>
2021-12-14 12:49:04 +0700

    Add Interrupt Priority

---
+++ Bai2_Interrupt_Priority/MDK-ARM/RTE/_ThiGK_TKHTN_Cau4/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ThiGK_TKHTN_Cau4' 
+ * Target:  'ThiGK_TKHTN_Cau4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
